803366758c446ae2b8125d9d445fffd87d484b5e
Gabriel <gabriel1997.castro@gmail.com>
2022-02-13 12:43:09 -0300

    Adiciona cliente http e mock de apis

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { api } from '../../services/apis';
 import { Container } from './styles';
 
 export function TransactionsTable() {
+
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, [])
+
   return (
     <Container>
       <table>
