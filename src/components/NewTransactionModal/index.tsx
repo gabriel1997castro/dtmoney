55903f0d25c05b25f99441960b3ffc20b125e8a3
Gabriel <gabriel1997.castro@gmail.com>
2022-03-01 12:53:50 -0300

    Cria transação dentro de banco fake no mirage

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { useState } from 'react';
+import { api } from '../../services/apis';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,7 +12,18 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('')
   const [type, setType] = useState('deposit');
+
+
+
+  function handleCreateNewTransaction(event: FormEvent){
+    event.preventDefault();
+    const data = { title, value, category, type };
+    api.post('/transactions', data)
+  }
 
   return (
     <Modal
@@ -23,16 +35,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       <button type="button" onClick={onRequestClose} className="react-modal-close">
         <img src={closeImg} alt="Fechar modal" />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar informação</h2>
 
 
         <input
           placeholder="Título"
+          value={title}
+          onChange={(event => setTitle(event.target.value))}
         />
 
         <input
           placeholder="Valor"
+          value={value}
+          onChange={(event => setValue(Number(event.target.value)))}
           type="number"
         />
 
@@ -59,6 +75,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
         <input
           placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
         />
 
         <button type="submit">
